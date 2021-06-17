0f5ce934a53d561bea38545b46d66d5d80d9a7c9
andrew21-mch <nfonandrew73@gmail.com>
2021-06-17 08:39:53 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/*Program to print using puts in c*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
