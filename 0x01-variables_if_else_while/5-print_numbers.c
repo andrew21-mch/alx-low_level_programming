57f17b4248e83c550dc15f7d9e6e61d4e2c5f7d1
andrew21-mch <nfonandrew73@gmail.com>
2021-06-18 18:28:51 +0100

    print digit from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 10
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+n=0;
+while(n<10)
+{
+printf("%d\n",n);
+n++;
+}
+printf("\n");
+return (0);
+}
