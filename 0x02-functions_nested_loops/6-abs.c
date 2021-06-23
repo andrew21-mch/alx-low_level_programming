9f825484edcbd2138f1ff889826e4883731eda68
andrew21-mch <nfonandrew73@gmail.com>
2021-06-23 15:00:55 -0700

    adding absolute functon

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int _abs(int)
+{
+  int r;
+
+  if(r>=0)
+    {
+      return r;
+    }
+  else
+    {
+      return -1*r;
+    }
+}
