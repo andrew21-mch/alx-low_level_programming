9c6bf720c1b8dbb0a9b60469d52f06148426e003
andrew21-mch <nfonandrew73@gmail.com>
2021-06-23 04:50:08 -0700

    alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  char ch;
+  ch = 'a';
+
+  while(ch<='z')
+    {
+      _putchar(ch);
+      ch++;
+    }
+  return (0);
+}
