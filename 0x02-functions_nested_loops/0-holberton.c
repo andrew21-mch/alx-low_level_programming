86ac816f3cc31a81e6a932df4a2160feba9b9687
andrew21-mch <nfonandrew73@gmail.com>
2021-06-23 04:38:21 -0700

    changes

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,22 @@
-#include "holberton.h"
+include "holberton.h"
+
+/**
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
-/* main -  prints Holberton, followed by a new line
-* Return: Always 0 (Success)
-*/
+
 {
-  _putchar("H");
-  _putchar("o");
-  _putchar("l");
-  _putchar("b");
-  _putchar("e");
-  _putchar("r");
-  _putchar("t");
-  _putchar("o");
-  _putchar("n");
-  _putchar("\n");
-  
-return 0;
+  _putchar('H');
+  _putchar('o');
+  _putchar('l');
+  _putchar('b');
+  _putchar('e');
+  _putchar('r');
+  _putchar('t');
+  _putchar('o');
+  _putchar('n');
+  _putchar('\n');
+
+  return (0);
 }
