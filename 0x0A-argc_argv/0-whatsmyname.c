c247882f14e3eed7f417a66e234b53ef7095b455
andrew21-mch <nfonandrew73@gmail.com>
2021-07-14 01:59:48 +0100

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
+#include <stdio.h>
 #include "holberton.h"
-#include<stdio.h>
 
-int main(int argc __attribute__((unused)), char *argv)
+/**
+ * main - prints program's name follwed by newline
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: always (0)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
-    /* main - prints program's name follwed by newline
-    * @argc: The number of command line arguments
-    * @argv: An array containing the program command line arguments
-    *
-    * Return: always (0)
-    */
-    printf("%s",argv[1]);
-    return 0;
+	printf("%s\n", *argv);
+
+	return (0);
 }
