1ed79ee42ca0b720c00bb993e93f78cb0fbaff28
andrew21-mch <nfonandrew73@gmail.com>
2021-07-14 02:19:49 +0100

    print the argument given line after another

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: (0) always
+ */
+int main(int argc, char *argv[])
+{
+    for(int i=0; i<=argc-1; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+}
