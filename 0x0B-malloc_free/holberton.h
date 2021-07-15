d31eeca9b3974d87586fa0e1ce85262c28800d29
andrew21-mch <nfonandrew73@gmail.com>
2021-07-15 23:54:49 +0100

    header file

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,10 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdio.h>
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
-void positive_or_negative(int i);
-int largest_number(int a , int b , int c);
-void print_remaining_days(int month ,int day ,int year);
-int convert_day(int month , int day);
-#endif /* HOLBERTON_H */
+#endif
+
