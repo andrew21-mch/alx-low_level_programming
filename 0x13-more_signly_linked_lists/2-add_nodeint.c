297100d3a13a83bc1ca450b85876cc45306cdd2c
andrew21-mch <nfonandrew73@gmail.com>
2021-08-03 21:38:08 +0100

    adds a new node at the beginning of a linklist

---
+++ 0x13-more_signly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_nodeint- adds a new node at the beginning of a linklist
+ * @head: pointer to the first node in the list
+ * @data: data to insert in that new node
+ * 
+ * return: pointer to the new node or Null if its fails
+ */
+
+listint_t *add_newint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
