fc36b156ac2134e0306d3100aeb758ae8c8248a6
andrew21-mch <nfonandrew73@gmail.com>
2021-09-24 13:52:40 -0700

    resolved conflict

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 gcc *.c -c -fPIC
-gcc *.o -shared -o libholberton.so
-=======
-gcc -g -fPIC -Wall -Werror -Wextra -pedantic *.c -shared -o liball.so
->>>>>>> faf0c102c099b75253f5bc9aa69836a8525a1f71
+gcc *.o -shared -o liball.so
