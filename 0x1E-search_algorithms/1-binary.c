c775574d8011556f28a673f7afe92a8cb13a4f36
andrew21-mch <nfonandrew73@gmail.com>
2022-01-20 00:09:53 +0100

    multiple

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -30,15 +30,16 @@ size_t pivot = 0, index = 0, high = 0;
 if (size == 0 || !array)
 return (-1);
 high = size - 1;
-while (high >= index){
-    display(array, index, high);
-    pivot = (high + index) / 2;
-    if (value == array[pivot])
-        return (pivot);
-    else if (value < array[pivot])
-        high = pivot - 1;
-    else
-        index = pivot + 1;
-    }
+while (high >= index)
+{
+display(array, index, high);
+pivot = (high + index) / 2;
+if (value == array[pivot])
+return (pivot);
+else if (value < array[pivot])
+high = pivot - 1;
+else
+index = pivot + 1;
+}
 return (-1);
 }
